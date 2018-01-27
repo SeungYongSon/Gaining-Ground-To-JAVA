91129b122d49cdd7837895f76947121893f31db6
Sungyong Son <space010127@gmail.com>
2018-01-28 00:09:33 +0900

    2018. 01. 28. Upload The Correct File

--- 2018-School-Vacation-Homework/C7020210/C7020210.java
+++ 2018-School-Vacation-Homework/C7020210/C7020210.java
@@ -1,7 +1,16 @@
 package C7020210;
 
+/*
+
+    410p. 챕터 7 실습문제 2번
+
+    Scanner 클래스를 사용하여 5개의 학점('A', 'B', 'C', 'D', 'F')을 문자로 입력받아 ArrayList에 저장하고,
+    ArrayList를 검색하여 학점을 점수(A=4.0, B=3.0, C=2.0, D=1.0, F=0)로 변환하여
+    출력하는 프로그램을 작성하라.
+
+ */
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class C7020210 {
@@ -15,12 +24,16 @@ public class C7020210 {
         for(int i = 0; i< 5; i++){
             char tmp = in.next().charAt(0);
             if((tmp  >= 'A' && tmp <= 'D') || tmp == 'F')
-                al.add(in.next().charAt(0));
+                al.add(tmp);
+            else {
+                i--;
+                System.out.println("Try Again.");
+            }
         }
 
-        double score=0.0;
+        double score = 0.0;
 
-        for(int i=0; i<al.size(); i++) {
+        for(int i = 0; i< al.size(); i++) {
             char ch = al.get(i);
             switch(ch) {
                 case 'A' : score = 4.0; break;
